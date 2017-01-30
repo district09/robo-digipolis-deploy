5fe3f221a795c9a2f7eb7917bf852c7ffb1a668e
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2017-01-30 14:53:57 +0100

    Fixing code style issues.

--- src/Ssh/Auth/None.php
+++ src/Ssh/Auth/None.php
@@ -15,28 +15,20 @@ class None extends AbstractAuth
 
     public function authenticate($connection)
     {
-        switch ($connection) {
-            case $connection instanceof SSH2:
-                if (!$connection->login($this->user)) {
-                    throw new \RuntimeException(sprintf(
-                        "fail: unable to authenticate user '%s', using password: NO",
-                        $this->user
-                    ));
-                }
-                break;
-
-            case $connection instanceof \Robo\Task\Remote\Ssh:
-                $connection->user($this->user);
-                break;
-
-            default:
-                throw new \InvalidArgumentException(sprintf(
-                    'Argument 1 passed to %s must be an instance of \phpseclib\Net\SSH2 or \Robo\Task\Remote\Ssh, %s given.',
-                    static::class . '::' . __METHOD__,
-                    gettype($connection) == 'object'
-                      ? get_class($connection)
-                      : gettype($connection)
-                ));
+        if (!($connection instanceof SSH2)) {
+            throw new \InvalidArgumentException(sprintf(
+                'Argument 1 passed to %s must be an instance of \phpseclib\Net\SSH2, %s given.',
+                static::class . '::' . __METHOD__,
+                gettype($connection) == 'object'
+                  ? get_class($connection)
+                  : gettype($connection)
+            ));
+        }
+        if (!$connection->login($this->user)) {
+            throw new \RuntimeException(sprintf(
+                "fail: unable to authenticate user '%s', using password: NO",
+                $this->user
+            ));
         }
     }
 }
