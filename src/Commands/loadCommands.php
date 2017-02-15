309ab57e83ffbd57effe2cdac05874cf4f249d28
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2017-02-15 11:34:11 +0100

    Fix code style issues.

--- src/Commands/loadCommands.php
+++ src/Commands/loadCommands.php
@@ -5,10 +5,10 @@ namespace DigipolisGent\Robo\Task\Deploy\Commands;
 trait loadCommands
 {
     use DatabaseBackup, DatabaseRestore {
-      DatabaseBackup::defaultFileSystemConfig insteadof DatabaseRestore;
-      DatabaseBackup::defaultDbConfig insteadof DatabaseRestore;
-      DatabaseBackup::parseDrupalDbConfig insteadof DatabaseRestore;
-      DatabaseBackup::createDbTask insteadof DatabaseRestore;
+        DatabaseBackup::defaultFileSystemConfig insteadof DatabaseRestore;
+        DatabaseBackup::defaultDbConfig insteadof DatabaseRestore;
+        DatabaseBackup::parseDrupalDbConfig insteadof DatabaseRestore;
+        DatabaseBackup::createDbTask insteadof DatabaseRestore;
     }
     use PushPackage;
 }
