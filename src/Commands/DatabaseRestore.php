b52fa9129b89c66d549acf206f103acbe519b26b
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2017-02-22 12:07:35 +0100

    Fix for filesystems.

--- src/Commands/DatabaseRestore.php
+++ src/Commands/DatabaseRestore.php
@@ -28,6 +28,15 @@ trait DatabaseRestore
         $source = is_null($opts['source'])
             ? realpath(getcwd()) . '/project.tar.gz'
             : $opts['source'];
+        if (is_null($opts['file-system-config'])) {
+            $opts['file-system-config'] = [
+                $opts['source-type'] => [
+                    'type' => ucfirst($opts['source-type']),
+                    'root' => realpath(dirname($source)),
+                ],
+            ];
+            $source = basename($source);
+        }
         return $this->createDbTask('taskDatabaseRestore', $database, $opts)
             ->source($source, $opts['source-type'])
             ->run();
