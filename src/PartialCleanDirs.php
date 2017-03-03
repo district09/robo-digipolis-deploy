c63c3969b5d756e62ffc0b70fff4f417e2928b58
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2017-03-03 16:14:39 +0100

    Fix PartialCleanDir

--- src/PartialCleanDirs.php
+++ src/PartialCleanDirs.php
@@ -245,16 +245,25 @@ class PartialCleanDirs extends BaseTask
         if ($keep) {
             array_splice($items, -$keep);
         }
-        foreach ($items as $item) {
+        while ($items) {
+            $item = reset($items);
             try {
                 // To delete a file we must have access rights on the parent
                 // directory.
                 $this->fs->chmod(dirname(realpath($item)), 0777, 0000, true);
                 $this->fs->chmod($item, 0777, 0000, true);
             } catch (IOException $e) {
-                // If chmod didn't work, try to remove anyway.
+                // If chmod didn't work and the exception contains a path, try
+                // to remove anyway.
+                $path = $e->getPath();
+                if ($path && realpath($path) !== realpath($item)) {
+                    $this->fs->remove($path);
+                    continue;
+                }
+
             }
             $this->fs->remove($item);
+            array_shift($items);
         }
     }
 }
