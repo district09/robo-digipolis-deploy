7cf826cb9e97d44fa4ec4b1d647c70f36e262897
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2023-04-13 11:02:27 +0200

    Remove stackspace adapter

--- src/BackupManager/Factory/FilesystemProviderFactory.php
+++ src/BackupManager/Factory/FilesystemProviderFactory.php
@@ -9,7 +9,6 @@ use District09\BackupManager\Filesystems\FilesystemProvider;
 use District09\BackupManager\Filesystems\FtpFilesystem;
 use District09\BackupManager\Filesystems\GcsFilesystem;
 use District09\BackupManager\Filesystems\LocalFilesystem;
-use District09\BackupManager\Filesystems\RackspaceFilesystem;
 use District09\BackupManager\Filesystems\SftpFilesystem;
 
 class FilesystemProviderFactory implements FilesystemProviderFactoryInterface
@@ -32,7 +31,6 @@ class FilesystemProviderFactory implements FilesystemProviderFactoryInterface
         $filesystemProvider->add(new DropboxFilesystem());
         $filesystemProvider->add(new FtpFilesystem());
         $filesystemProvider->add(new LocalFilesystem());
-        $filesystemProvider->add(new RackspaceFilesystem());
         $filesystemProvider->add(new SftpFilesystem());
 
         return $filesystemProvider;
